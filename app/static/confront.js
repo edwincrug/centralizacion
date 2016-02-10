87c700d478faf238110e987d8e0df11334e63be7
edwincrug <edwincrug@gmail.com>
2016-02-10 12:24:37 -0600

    Cambio inclusión nodo.css

--- app/static/confront.js
+++
@@ -1,9 +0,0 @@
-/*var global_settings = {
-	urlCORS: 'http://192.168.20.9/ApiCentral',
-	uploadPath: 'C:/Desarrollo/Central/centralizacion/app/uploads'
-} */ 
-var global_settings = {
-	urlCORS:  'http://localhost:60582',
-	uploadPath: 'C:/Desarrollo/Central/centralizacion/app/uploads',
-	nodoSaltoRefacciones: [4,6]
-} 
