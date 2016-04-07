33ebce1a7b0d28b4ffa75c3168514803f605c3da
edwincrug <edwincrug@gmail.com>
2016-04-07 14:11:52 -0500

    Corrijo nombre de html en índex NODE

--- app/expressServer.js
+++ app/expressServer.js
@@ -83,7 +83,7 @@ var ExpressServer = function(config){
 
     //Servimos el archivo angular
     this.expressServer.get('*', function(req, res){
-        res.sendfile('app/static/index.htm');
+        res.sendfile('app/static/index.html');
     });
 
     //Recibo las variables de login
